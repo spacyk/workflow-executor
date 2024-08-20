16d07fd9d5e4c1ee9c36ac611e0fedab19653340
Jan Spacek <jan.spacek12@gmail.com>
2024-08-20 14:36:48 +0200

    improve tests

--- test/workflowExecutor.test.ts
+++ test/workflowExecutor.test.ts
@@ -2,9 +2,10 @@
 
 import { WorkflowExecutor } from '../src/workflowExecutor';
 import type { Workflow } from '../src/workflow.interface';
+import Mock = jest.Mock;
 
 describe('WorkflowExecutor.nodeById', () => {
-  it('test getting node by ID', () => {
+  it('get node by ID', () => {
     const workflow: Workflow = {
       nodes: [
         { id: 'A', execute: () => {} },
@@ -25,7 +26,7 @@ describe('WorkflowExecutor.nodeById', () => {
 });
 
 describe('WorkflowExecutor.getStartingNode', () => {
-  it('test get starting node', () => {
+  it('get starting node for the workflow', () => {
     const workflow: Workflow = {
       nodes: [
         { id: 'A', execute: () => {} },
@@ -46,38 +47,15 @@ describe('WorkflowExecutor.getStartingNode', () => {
 });
 
 describe('WorkflowExecutor.execute', () => {
-  it('test execute with $next path specified, [A, C, E]', () => {
-    const mockFunction = jest.fn();
-    const workflow: Workflow = {
-      nodes: [
-        {
-          id: 'A',
-          execute: () => {
-            mockFunction('A');
-            return { $next: 'false' };
-          },
-        },
-        { id: 'B', execute: () => mockFunction('B') },
-        { id: 'C', execute: () => mockFunction('C') },
-        { id: 'D', execute: () => mockFunction('D') },
-        { id: 'E', execute: () => mockFunction('E') },
-      ],
-      edges: [
-        { name: 'true', from: 'A', to: 'B' },
-        { name: 'false', from: 'A', to: 'C' },
-        { from: 'B', to: 'D' },
-        { from: 'D', to: 'E' },
-        { from: 'C', to: 'E' },
-      ],
-    };
-
-    const workflowExecutor = new WorkflowExecutor(workflow);
-    workflowExecutor.execute();
-    expect(mockFunction.mock.calls).toEqual([['A'], ['C'], ['E']]);
+  let mockFunction: Mock;
+  beforeEach(() => {
+    mockFunction = jest.fn();
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  it('test execute with multiple valid edges for one node, [A, B, C, D]', () => {
-    const mockFunction = jest.fn();
+  it('verify each node executes just once even with multiple valid edges.', () => {
     const workflow: Workflow = {
       nodes: [
         { id: 'A', execute: () => mockFunction('A') },
@@ -98,85 +76,80 @@ describe('WorkflowExecutor.execute', () => {
     expect(mockFunction.mock.calls).toEqual([['A'], ['B'], ['C'], ['D']]);
   });
 
-  it('workflow with cycle. Cycle nodes not executed by default [A, B, C, D]', () => {
-    const mockFunction = jest.fn();
-    const workflow: Workflow = {
-      nodes: [
-        { id: 'A', execute: () => mockFunction('A') },
-        { id: 'B', execute: () => mockFunction('B') },
-        { id: 'C', execute: () => mockFunction('C') },
-        { id: 'D', execute: () => mockFunction('D') },
-      ],
-      edges: [
-        { from: 'A', to: 'B' },
-        { from: 'B', to: 'C' },
-        { from: 'C', to: 'D' },
-        { from: 'D', to: 'C' },
-      ],
-    };
+  it.each([
+    [0, ['A', 'B', 'C']],
+    [1, ['A', 'B', 'C', 'B', 'C']],
+    [2, ['A', 'B', 'C', 'B', 'C', 'B', 'C']],
+  ])(
+    'verify workflow execution with cycle. cyclesAllowed=%s',
+    (cyclesAllowed, expected) => {
+      const workflow: Workflow = {
+        nodes: [
+          { id: 'A', execute: () => mockFunction('A') },
+          { id: 'B', execute: () => mockFunction('B') },
+          { id: 'C', execute: () => mockFunction('C') },
+        ],
+        edges: [
+          { from: 'A', to: 'B' },
+          { from: 'B', to: 'C' },
+          { from: 'C', to: 'B' },
+        ],
+      };
 
-    const workflowExecutor = new WorkflowExecutor(workflow);
-    workflowExecutor.execute();
-    expect(mockFunction.mock.calls).toEqual([['A'], ['B'], ['C'], ['D']]);
-  });
+      const workflowExecutor = new WorkflowExecutor(workflow, cyclesAllowed);
+      workflowExecutor.execute();
+      expect(mockFunction.mock.calls.map((value) => value[0])).toEqual(
+        expected,
+      );
+    },
+  );
 
-  it('workflow with cycle. Allow 1 cycle [A, B, C, D, C, D]', () => {
-    const mockFunction = jest.fn();
-    const workflow: Workflow = {
-      nodes: [
-        { id: 'A', execute: () => mockFunction('A') },
-        { id: 'B', execute: () => mockFunction('B') },
-        { id: 'C', execute: () => mockFunction('C') },
-        { id: 'D', execute: () => mockFunction('D') },
-      ],
-      edges: [
-        { from: 'A', to: 'B' },
-        { from: 'B', to: 'C' },
-        { from: 'C', to: 'D' },
-        { from: 'D', to: 'C' },
-      ],
-    };
+  it.each([
+    ['false', 'false', 0, ['A', 'B']],
+    ['true', 'false', 0, ['A', 'C', 'E']],
+    ['true', 'true', 0, ['A', 'C', 'D', 'F', 'G']],
+    ['true', 'true', 1, ['A', 'C', 'D', 'F', 'G', 'D', 'F', 'G']],
+  ])(
+    'verify complex workflow execution with edge conditions ($next) and cycle. A=%s, C=%s, cyclesAllowed=%s',
+    (returnValueA, returnValueC, cyclesAllowed, expected) => {
+      const workflow: Workflow = {
+        nodes: [
+          {
+            id: 'A',
+            execute: () => {
+              mockFunction('A');
+              return { $next: returnValueA };
+            },
+          },
+          { id: 'B', execute: () => mockFunction('B') },
+          {
+            id: 'C',
+            execute: () => {
+              mockFunction('C');
+              return { $next: returnValueC };
+            },
+          },
+          { id: 'D', execute: () => mockFunction('D') },
+          { id: 'E', execute: () => mockFunction('E') },
+          { id: 'F', execute: () => mockFunction('F') },
+          { id: 'G', execute: () => mockFunction('G') },
+        ],
+        edges: [
+          { from: 'A', to: 'B', name: 'false' },
+          { from: 'A', to: 'C', name: 'true' },
+          { from: 'C', to: 'D', name: 'true' },
+          { from: 'C', to: 'E', name: 'false' },
+          { from: 'D', to: 'F' },
+          { from: 'F', to: 'G' },
+          { from: 'G', to: 'D' },
+        ],
+      };
 
-    const workflowExecutor = new WorkflowExecutor(workflow, 1);
-    workflowExecutor.execute();
-    expect(mockFunction.mock.calls).toEqual([
-      ['A'],
-      ['B'],
-      ['C'],
-      ['D'],
-      ['C'],
-      ['D'],
-    ]);
-  });
-
-  it('workflow with cycle. Allow 2 cycles [A, B, C, D, C, D, C, D]', () => {
-    const mockFunction = jest.fn();
-    const workflow: Workflow = {
-      nodes: [
-        { id: 'A', execute: () => mockFunction('A') },
-        { id: 'B', execute: () => mockFunction('B') },
-        { id: 'C', execute: () => mockFunction('C') },
-        { id: 'D', execute: () => mockFunction('D') },
-      ],
-      edges: [
-        { from: 'A', to: 'B' },
-        { from: 'B', to: 'C' },
-        { from: 'C', to: 'D' },
-        { from: 'D', to: 'C' },
-      ],
-    };
-
-    const workflowExecutor = new WorkflowExecutor(workflow, 2);
-    workflowExecutor.execute();
-    expect(mockFunction.mock.calls).toEqual([
-      ['A'],
-      ['B'],
-      ['C'],
-      ['D'],
-      ['C'],
-      ['D'],
-      ['C'],
-      ['D'],
-    ]);
-  });
+      const workflowExecutor = new WorkflowExecutor(workflow, cyclesAllowed);
+      workflowExecutor.execute();
+      expect(mockFunction.mock.calls.map((value) => value[0])).toEqual(
+        expected,
+      );
+    },
+  );
 });
--- test/workflowValidator.test.ts
+++ test/workflowValidator.test.ts
@@ -1,53 +1,54 @@
 import { WorkflowValidator } from '../src/workflowValidator';
+import { Edge, Node } from '../src/workflow.interface';
 
 describe('WorkflowValidator.validate', () => {
   it.each([
-    {
-      nodes: [],
-      edges: [],
-    },
-    {
-      nodes: [
+    [[], []],
+    [
+      [
         { id: 'A', execute: () => {} },
         { id: 'B', execute: () => {} },
       ],
-      edges: [],
-    },
-    {
-      nodes: [
+      [],
+    ],
+    [
+      [
         { id: 'A', execute: () => {} },
         { id: 'B', execute: () => {} },
         { id: 'C', execute: () => {} },
       ],
-      edges: [{ from: 'A', to: 'B' }],
+      [{ from: 'A', to: 'B' }],
+    ],
+  ])(
+    'validate invalid workflow, number of starting nodes !== 1',
+    (nodes: Node[], edges: Edge[]) => {
+      // Execute
+      expect(() => {
+        WorkflowValidator.validate({ nodes, edges });
+      }).toThrow(
+        'There is exactly one starting node allowed in the workflow. The workflow is invalid.',
+      );
     },
-  ])('test invalid workflow, number of starting nodes !== 1', (workflow) => {
-    // Execute
-    expect(() => {
-      WorkflowValidator.validate(workflow);
-    }).toThrow(
-      'There is exactly one starting node allowed in the workflow. The workflow is invalid.',
-    );
-  });
+  );
 
   it.each([
-    {
-      nodes: [{ id: 'A', execute: () => {} }],
-      edges: [],
-    },
-    {
-      nodes: [
+    [[{ id: 'A', execute: () => {} }], []],
+    [
+      [
         { id: 'A', execute: () => {} },
         { id: 'B', execute: () => {} },
       ],
-      edges: [{ from: 'A', to: 'B' }],
+      [{ from: 'A', to: 'B' }],
+    ],
+  ])(
+    'validate valid workflow, number of starting nodes === 1',
+    (nodes: Node[], edges: Edge[]) => {
+      // Execute
+      expect(() => {
+        WorkflowValidator.validate({ nodes, edges });
+      }).not.toThrow(
+        'There is exactly one starting node allowed in the workflow. The workflow is invalid.',
+      );
     },
-  ])('test valid workflow, number of starting nodes === 1', (workflow) => {
-    // Execute
-    expect(() => {
-      WorkflowValidator.validate(workflow);
-    }).not.toThrow(
-      'There is exactly one starting node allowed in the workflow. The workflow is invalid.',
-    );
-  });
+  );
 });
